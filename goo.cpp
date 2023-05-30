4c00b61b0bbf0a878e5511d86405a88570ca93b1
Mirek Zielinski <mirek@parasoft.com>
2023-05-30 10:33:01 -0400

    Initial commit

---
+++ goo.cpp
@@ -0,0 +1,35 @@
+#include "goo.h"
+
+int getValue(int a, int b)
+{
+    if ((a > 0) || (b > 0)) {
+        return a * b;
+    }
+
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+
+    return 0;   //parasoft-cov-suppress LC "Legacy code - approved"
+} 
+
+int findGCD(int n1, int n2) {
+  int hcf = 0;
+  
+  if ( n2 > n1) {   
+    int temp = n2;
+    n2 = n1;
+    n1 = temp;
+  }
+    
+  for (int i = 1; i <=  n2; ++i) {
+    if (n1 % i == 0 && n2 % i ==0) {
+      
+      hcf = i;
+    }
+  }
+
+  return hcf;
+}
---
+++ goo.h
@@ -0,0 +1,2 @@
+int getValue(int a, int b);
+int findGCD(int n1, int n2);
